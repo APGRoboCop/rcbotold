27e37c3ac81eed3c4d7c373816acf79684f9c9aa
R. Hunter <robocop495@gmail.com>
2026-09-07 11:30:54 +0100

    Update bot.cpp

--- dlls/bot.cpp
+++ dlls/bot.cpp
@@ -1786,10 +1786,11 @@ void CBot::SpawnInit(const bool bInit)
 
 	m_fLastThinkTime = 0.0f;
 
-	m_vStuckSampleOrigin = Vector(0, 0, 0);
-	m_fStuckSampleTime = 0.0f;
-	m_fMeasured3dSpeed = 0.0f;
-	m_fMeasured2dSpeed = 0.0f;
+	//m_vStuckSampleOrigin = Vector(0, 0, 0);
+	//m_fStuckSampleTime = 0.0f;
+	//m_fMeasured3dSpeed = 0.0f;
+	//m_fMeasured2dSpeed = 0.0f;
+
 	m_fUpdateWaypointTime = 0.0f;
 
 	//    m_pSound = NULL;
